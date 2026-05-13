6cad1ecacd28c6bd05409519c6110472ad492094
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-05-13 12:46:01 +0100

    Added images folder and made modifications
    Added link.svg for better UI viewing when looking at a blog
    Added more box-shadow improvements still to work on this.
    
    Added overall better UI improvements.

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 function githubRedirect() {
-    let userAnswer = prompt("Would you like to redirect?");
-    let userAnswerLowercase = userAnswer.toLowerCase();
-    if(userAnswerLowercase === "yes" || userAnswerLowercase === "") {
-        window.location.href="https://github.com/Ultrabugs";
-    }  
+    window.location.href="https://github.com/Ultrabugs";
 }
 
 const bewgsyWaterMark = document.querySelector('.bewgsy');
